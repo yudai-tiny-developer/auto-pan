2f89d62c177160328133cbef5ba2c5c52cba50e1
yudai-tiny-developer <yudai-tiny-developer@gmail.com>
2022-07-28 03:13:32 +0900

    stop

--- background.js
+++ background.js
@@ -2,16 +2,16 @@
 
 let center_x;
 let center_y;
-let tabId_to_panner = new Map();
+let tabId_to_audio = new Map();
 let windowId_to_tabId = new Map();
 const context = new AudioContext();
 
 function capture(tabId, windowId, create, type = 'StereoPan') {
-	if (tabId_to_panner.has(tabId)) {
+	if (tabId_to_audio.has(tabId)) {
 		pan(tabId, windowId, type);
 	} else if (create) {
 		chrome.tabCapture.capture({ audio: true, video: false }, (stream) => {
-			addPanner(tabId, stream, type);
+			addAudio(tabId, stream, type);
 			pan(tabId, windowId, type);
 		});
 	}
@@ -19,15 +19,15 @@ function capture(tabId, windowId, create, type = 'StereoPan') {
 
 function pan(tabId, windowId, type) {
 	chrome.windows.get(windowId, window => {
-		const panner = tabId_to_panner.get(tabId);
+		const audio = tabId_to_audio.get(tabId);
 		switch (type) {
 			case 'Pan':
-				panner.positionX.setTargetAtTime((window.left + window.width / 2 - center_x) / center_x * 4, context.currentTime, 0.2);
-				panner.positionY.setTargetAtTime(-(window.top + window.height / 2 - center_y) / center_y * 8, context.currentTime, 0.2);
-				panner.positionZ.value = -1;
+				audio.panner.positionX.setTargetAtTime((window.left + window.width / 2 - center_x) / center_x * 4, context.currentTime, 0.2);
+				audio.panner.positionY.setTargetAtTime(-(window.top + window.height / 2 - center_y) / center_y * 8, context.currentTime, 0.2);
+				audio.panner.positionZ.value = -1;
 				break;
 			case 'StereoPan':
-				panner.pan.setTargetAtTime(Math.min(1, Math.max(-1, (window.left + window.width / 2 - center_x) / center_x) * 1.25), context.currentTime, 0.2);
+				audio.panner.pan.setTargetAtTime(Math.min(1, Math.max(-1, (window.left + window.width / 2 - center_x) / center_x) * 1.25), context.currentTime, 0.2);
 				break;
 		}
 	});
@@ -53,7 +53,7 @@ function removeMap(tabId, windowId) {
 	}
 }
 
-function addPanner(tabId, stream, type = 'StereoPan') {
+function addAudio(tabId, stream, type = 'StereoPan') {
 	let panner;
 	switch (type) {
 		case 'Pan':
@@ -66,14 +66,25 @@ function addPanner(tabId, stream, type = 'StereoPan') {
 			break;
 	}
 
-	context.createMediaStreamSource(stream).connect(panner);
+	const source = context.createMediaStreamSource(stream);
+	source.connect(panner);
 	panner.connect(context.destination);
 
-	tabId_to_panner.set(tabId, panner);
+	tabId_to_audio.set(tabId, {
+		stream: stream,
+		source: source,
+		panner: panner,
+	});
 }
 
-function removePanner(tabId) {
-	tabId_to_panner.delete(tabId);
+function removeAudio(tabId) {
+	const audio = tabId_to_audio.get(tabId);
+	if (audio) {
+		for (const track of audio.stream.getTracks()) {
+			track.stop();
+		}
+		tabId_to_audio.delete(tabId);
+	}
 }
 
 chrome.system.display.getInfo({}, displayInfo => {
@@ -92,19 +103,29 @@ chrome.windows.onBoundsChanged.addListener(window => {
 
 chrome.contextMenus.create({
 	id: 'hkghcgebakholhmhijhpipamdmbcnbgj',
-	title: 'Enable Auto Pan',
+	title: 'Auto Pan',
 	type: 'normal',
 	contexts: ['page', 'frame', 'image', 'video']
 }, () => { });
 
 chrome.contextMenus.onClicked.addListener((info, tab) => {
-	addMap(tab.id, tab.windowId);
-	capture(tab.id, tab.windowId, true);
+	if (tabId_to_audio.has(tab.id)) {
+		removeMap(tab.id, tab.windowId);
+		removeAudio(tab.id);
+	} else {
+		addMap(tab.id, tab.windowId);
+		capture(tab.id, tab.windowId, true);
+	}
 });
 
 chrome.browserAction.onClicked.addListener(tab => {
-	addMap(tab.id, tab.windowId);
-	capture(tab.id, tab.windowId, true);
+	if (tabId_to_audio.has(tab.id)) {
+		removeMap(tab.id, tab.windowId);
+		removeAudio(tab.id);
+	} else {
+		addMap(tab.id, tab.windowId);
+		capture(tab.id, tab.windowId, true);
+	}
 });
 
 chrome.tabs.onAttached.addListener((tabId, attachInfo) => {
@@ -118,7 +139,7 @@ chrome.tabs.onDetached.addListener((tabId, detachInfo) => {
 
 chrome.tabs.onRemoved.addListener((tabId, removeInfo) => {
 	removeMap(tabId, removeInfo.windowId);
-	removePanner(tabId);
+	removeAudio(tabId);
 });
 
 chrome.windows.onRemoved.addListener(windowId => {
