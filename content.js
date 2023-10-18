7a901a852ef833a6533432235429b4f9d485e112
yudai-tiny-developer <yudai-tiny-developer@gmail.com>
2023-10-19 01:53:35 +0900

    if (panner)

--- content.js
+++ content.js
@@ -54,7 +54,9 @@ import(chrome.runtime.getURL('common.js')).then(common => {
                 }, 100);
             }, 100);
         } else {
-            panner.pan.value = 0;
+            if (panner) {
+                panner.pan.value = 0;
+            }
         }
     }
 
