f2e87311be15e85e7e314bf3f7db267f2e6893fe
Simple and Tiny Utilities <108601122+yudai-tiny-developer@users.noreply.github.com>
2024-02-25 06:30:18 +0900

    V1.8.0 (#32)
    * reset ui
    
    * width
    
    * center, prefers-color-scheme
    
    * title
    
    * margin, color
    
    * prefers-color-scheme
    
    * progress module
    
    * settings
    
    * class
    
    * remove console
    
    * comp
    
    * preventDefault
    
    ---------
    
    Co-authored-by: yudai-tiny-developer <yudai-tiny-developer@gmail.com>

--- popup.js
+++ popup.js
@@ -1,110 +1,62 @@
-import(chrome.runtime.getURL('common.js')).then(common => {
-    chrome.storage.local.get(common.storage, data => {
-        const container = document.querySelector('div#container');
+import(chrome.runtime.getURL('common.js')).then(common =>
+    import(chrome.runtime.getURL('settings.js')).then(settings =>
+        import(chrome.runtime.getURL('progress.js')).then(progress =>
+            chrome.storage.local.get(common.storage, data =>
+                main(common, settings, progress, data)
+            )
+        )
+    )
+);
 
-        createRow(container, row => {
-            row.appendChild(createLabel('Enabled/Disabled'));
-            row.appendChild(createToggle('enabled', data.enabled, common.defaultEnabled));
-        });
+function main(common, settings, progress, data) {
+    const row_class = 'row';
+    const cell_class = 'cell';
+    const toggle_class = 'toggle';
+    const label_class = 'switch';
+    const input_class = 'rate';
+    const progress_class = 'progress';
+    const done_class = 'done';
 
-        createRow(container, row => {
-            row.appendChild(createLabel(`Pan Rate (${common.minPanRate.toFixed(1)} ~ ${common.maxPanRate.toFixed(1)})`));
-            row.appendChild(createInput('panRate', data.panRate, common.defaultPanRate, common.minPanRate, common.maxPanRate, common.stepPanRate));
-        });
+    const container = document.querySelector('div#container');
+    const reset_button = document.querySelector('input#reset');
+    const progress_div = document.querySelector('div#reset_progress');
 
-        createRow(container, row => {
-            row.appendChild(createLabel());
-            row.appendChild(createLabel(`${common.maxPanRate.toFixed(1)}: Rapidly panning<br>${common.minPanRate.toFixed(1)}: Gradually panning`));
-        });
-
-        createRow(container, row => {
-            row.appendChild(createLabel('Use vertical'));
-            row.appendChild(createToggle('pan2d', data.pan2d, common.defaultpPan2d));
-        });
-
-        createRow(container, row => {
-            row.appendChild(createLabel('Smooth panning (High CPU load)'));
-            row.appendChild(createToggle('smooth', data.smooth, common.defaultSmooth));
-        });
-
-        createRow(container, row => {
-            row.appendChild(createLabel(`Smoothing Interval (${common.minSmoothRate.toFixed(0)} ms ~ ${common.maxSmoothRate.toFixed(0)} ms)`));
-            row.appendChild(createInput('smoothRate', data.smoothRate, common.defaultSmoothRate, common.minSmoothRate, common.maxSmoothRate, common.stepSmoothRate));
-        });
-
-        createRow(container, row => {
-            row.appendChild(createLabel());
-            row.appendChild(createLabel(`${common.maxSmoothRate.toFixed(0)} ms: Lower load<br>${common.minSmoothRate.toFixed(0)} ms: Higher load`));
-        });
-    });
-
-    function createRow(container, gen) {
-        const row = document.createElement('div');
-        row.classList.add('row');
-        gen(row);
+    {
+        const row = settings.createRow(row_class);
+        row.appendChild(settings.createLabel(cell_class, 'Enabled/Disabled'));
+        row.appendChild(settings.createToggle(cell_class, toggle_class, label_class, 'enabled', data.enabled, common.defaultEnabled, common.value));
+        container.appendChild(row);
+    } {
+        const row = settings.createRow(row_class);
+        row.appendChild(settings.createLabel(cell_class, `Pan Rate (${common.minPanRate.toFixed(1)} ~ ${common.maxPanRate.toFixed(1)})`));
+        row.appendChild(settings.createNumberStepInput(cell_class, input_class, 'panRate', data.panRate, common.defaultPanRate, common.minPanRate, common.maxPanRate, common.stepPanRate, common.limitRate));
+        container.appendChild(row);
+    } {
+        const row = settings.createRow(row_class);
+        row.appendChild(settings.createLabel(cell_class));
+        row.appendChild(settings.createLabel(cell_class, `${common.maxPanRate.toFixed(1)}: Rapidly panning<br>${common.minPanRate.toFixed(1)}: Gradually panning`));
+        container.appendChild(row);
+    } {
+        const row = settings.createRow(row_class);
+        row.appendChild(settings.createLabel(cell_class, 'Use vertical'));
+        row.appendChild(settings.createToggle(cell_class, toggle_class, label_class, 'pan2d', data.pan2d, common.defaultpPan2d, common.value));
+        container.appendChild(row);
+    } {
+        const row = settings.createRow(row_class);
+        row.appendChild(settings.createLabel(cell_class, 'Smooth panning (High CPU load)'));
+        row.appendChild(settings.createToggle(cell_class, toggle_class, label_class, 'smooth', data.smooth, common.defaultSmooth, common.value));
+        container.appendChild(row);
+    } {
+        const row = settings.createRow(row_class);
+        row.appendChild(settings.createLabel(cell_class, `Smoothing Interval (${common.minSmoothRate.toFixed(0)} ms ~ ${common.maxSmoothRate.toFixed(0)} ms)`));
+        row.appendChild(settings.createNumberStepInput(cell_class, input_class, 'smoothRate', data.smoothRate, common.defaultSmoothRate, common.minSmoothRate, common.maxSmoothRate, common.stepSmoothRate, common.limitRate));
+        container.appendChild(row);
+    } {
+        const row = settings.createRow(row_class);
+        row.appendChild(settings.createLabel(cell_class));
+        row.appendChild(settings.createLabel(cell_class, `${common.maxSmoothRate.toFixed(0)} ms: Lower load<br>${common.minSmoothRate.toFixed(0)} ms: Higher load`));
         container.appendChild(row);
     }
 
-    function createLabel(label = '') {
-        const cell = document.createElement('div');
-        cell.classList.add('cell');
-        cell.innerHTML = label;
-        return cell;
-    }
-
-    function createToggle(key, checked, defaultValue) {
-        const cell = document.createElement('div');
-        cell.classList.add('cell');
-
-        const input = document.createElement('input');
-        input.id = key;
-        input.classList.add('toggle');
-        input.type = 'checkbox';
-        input.checked = common.value(checked, defaultValue);
-        input.setAttribute('default', defaultValue);
-        input.addEventListener('change', () => {
-            chrome.storage.local.set({ [key]: input.checked });
-        });
-        cell.appendChild(input);
-
-        const label = document.createElement('label');
-        label.classList.add('switch');
-        label.setAttribute('for', key);
-        cell.appendChild(label);
-
-        return cell;
-    }
-
-    function createInput(key, value, defaultValue, minRate, maxRate, stepRate) {
-        const cell = document.createElement('div');
-        cell.classList.add('cell');
-
-        const input = document.createElement('input');
-        input.id = key;
-        input.classList.add('rate');
-        input.type = 'number';
-        input.value = common.limitRate(value, defaultValue, minRate, maxRate, stepRate);
-        input.setAttribute('default', defaultValue);
-        input.min = minRate;
-        input.max = maxRate;
-        input.step = stepRate;
-        input.addEventListener('change', () => {
-            chrome.storage.local.set({ [key]: common.limitRate(input.value, defaultValue, minRate, maxRate, stepRate) });
-        });
-        cell.appendChild(input);
-
-        return cell;
-    }
-
-    document.querySelector('input#reset').addEventListener('click', () => {
-        for (const input of document.querySelectorAll('input.toggle')) {
-            input.checked = input.getAttribute('default') === 'true';
-        }
-
-        for (const input of document.querySelectorAll('input.rate')) {
-            input.value = input.getAttribute('default');
-        }
-
-        chrome.storage.local.clear();
-    });
-});
+    settings.registerResetButton(reset_button, progress_div, progress_class, done_class, toggle_class, input_class, progress);
+}
