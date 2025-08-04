6adbf9933336e26e1b851980982f8a1802cce7ca
Simple and Tiny Utilities <108601122+yudai-tiny-developer@users.noreply.github.com>
2025-08-04 22:19:44 +0900

    Partial implementation of support for multi-monitor environments (#40)
    Co-authored-by: Simple and Tiny Utilities <yudai-tiny-developer@gmail.com>

--- background.js
+++ background.js
@@ -1,6 +1,25 @@
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    chrome.windows.get(sender.tab.windowId).then(response => {
-        sendResponse(response);
-    });
+    switch (message.msg) {
+        case 'GetCurrentWindow':
+            chrome.windows.get(sender.tab.windowId).then(response => {
+                sendResponse(response);
+            });
+            break;
+        case 'GetCenter':
+            chrome.windows.getAll().then(windows => {
+                let left = 0;
+                let top = 0;
+                let right = message.screen_width;
+                let bottom = message.screen_height;
+                for (const window of windows) {
+                    left = Math.min(left, window.left);
+                    top = Math.min(top, window.top);
+                    right = Math.max(right, window.left + window.width);
+                    bottom = Math.max(bottom, window.top + window.height);
+                }
+                sendResponse({ center_x: (right - left) / 2.0, center_y: (bottom - top) / 2.0 });
+            });
+            break;
+    }
     return true;
 });
