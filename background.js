2f43eace512e13cd507b324f3e3267901561cc59
Simple and Tiny Utilities <108601122+yudai-tiny-developer@users.noreply.github.com>
2025-09-17 20:45:59 +0900

    V1.14.1 (#44)
    * v1.14.1
    
    * multimonitor flag
    
    ---------
    
    Co-authored-by: Simple and Tiny Utilities <yudai-tiny-developer@gmail.com>

--- background.js
+++ background.js
@@ -1,3 +1,12 @@
+function isPointInBounds(x, y, bounds) {
+    return (
+        x >= bounds.left &&
+        x <= bounds.left + bounds.width &&
+        y >= bounds.top &&
+        y <= bounds.top + bounds.height
+    );
+}
+
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     switch (message.msg) {
         case 'GetCurrentWindow':
@@ -7,19 +16,14 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
             break;
         case 'GetCenter':
             chrome.system.display.getInfo().then(displays => {
-                let left = message.screen_left;
-                let top = message.screen_right;
-                let right = message.screen_left + message.screen_width;
-                let bottom = message.screen_right + message.screen_height;
-                if (message.multimonitor) {
-                    for (const display of displays) {
-                        left = Math.min(left, display.bounds.left);
-                        top = Math.min(top, display.bounds.top);
-                        right = Math.max(right, display.bounds.left + display.bounds.width);
-                        bottom = Math.max(bottom, display.bounds.top + display.bounds.height);
+                for (const display of displays) {
+                    if (display.isPrimary) {
+                        sendResponse({ center_x: display.bounds.left + display.bounds.width / 2.0, center_y: display.bounds.top + display.bounds.height / 2.0, width: display.bounds.width, height: display.bounds.height });
+                        return;
                     }
                 }
-                sendResponse({ center_x: (left + right) / 2.0, center_y: (top + bottom) / 2.0, width: Math.abs(left) + Math.abs(right), height: Math.abs(top) + Math.abs(bottom) });
+
+                sendResponse(undefined); // primary display not found
             });
             break;
     }
